672baaafa948f9d96049eedc40c6e8a224868c19
leoncozzi <leoncozzi@gmail.com>
2020-04-07 08:49:39 +0100

    Add try and except statement to catch errors on IPSet seen in customer lab,

---
+++ dfw-exportv6.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python
+# This Script is for demo purposes only. If to be adapted for production the users should test and validate operations.
+# Author leoncozzi@gmail.com
+
+import json
+from sys import argv
+import csv
+import sys
+import requests
+
+# If using a valid certificate, these two lines can and SHOULD be removed.
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+if len(argv) == 5:
+    script, filename, nsx_manager, nsx_username, nsx_password = argv
+else:
+    print('Syntax: dfw-export.py [target_file_name] [nsx_manager_ip_or_hostname] [nsx_username] [nsx_password]')
+    sys.exit()
+# Set HTTP parameters for REST API call
+# If you want to verify SSL, set nsx.verify = '/path/to/cert.pem'
+nsx = requests.Session()
+nsx.verify = False
+nsx.auth = (nsx_username, nsx_password)
+nsx.headers = {'Accept': 'application/json'}
+
+filename += '.csv'
+target = open(filename, 'w')
+target.truncate()
+csv_file = csv.writer(target)
+
+print ("CSV: Opened file", filename)
+
+# CSV Header
+csv_file.writerow(("Rule ID", "Name", "Services", "Applied To", "Action", "Direction", "Destination", "Destination Contents", "Sources", "Source Contents"))
+print
+"CSV: Added CSV header row.."
+# Get DFW Rulebase
+sections = nsx.get('https://' + nsx_manager + '/api/v1/firewall/sections')
+# Convert JSON response to python dict
+section = json.loads(sections.text)
+
+# print(section)
+# print rulebase
+# Iterate every layer 3 section in the rulebase
+for sec in section['results']:
+    # grab each section id in turn from the sections response
+    if 'id' in sec:
+       section_row = "Section:" + sec['display_name'] + ", ID: " + sec['id']
+       csv_file.writerow([section_row])
+       # Get all the rules for the section 'id'
+       rule = nsx.get('https://' + nsx_manager + '/api/v1/firewall/sections/' + sec['id'] + '/rules')
+       # Convert JSON response rule to python dict ruled
+       ruled = json.loads(rule.text)
+       for rule in ruled['results']:
+         this = dict()
+         this['destination includes'] = ' '
+         this['destination'] = 'ANY'
+         this['sources includes'] = ' '
+         this['sources'] = 'ANY'
+         this['id'] = ' '
+         this['display_name'] = ' '
+         this['services'] = ' '
+         this['applied_tos'] = ' '
+         this['action'] = ' '
+         this['direction'] = ' '
+         if rule.get('id'):
+             this['id'] = rule['id']
+         if rule.get('display_name'):
+             this['display_name'] = rule['display_name']
+         if rule.get('services'):
+             this['services'] = rule['services']
+         if rule.get('applied_tos'):
+             this['applied_tos'] = rule['applied_tos']
+         if rule.get('action'):
+             this['action'] = rule['action']
+         if rule.get('direction'):
+            this['direction'] = rule['direction']
+         if rule.get('destinations'):
+            if 'target_id' in rule['destinations'][0]:
+                this['destination'] = rule['destinations'][0]['target_display_name']
+                ipset = nsx.get('https://' + nsx_manager + '/api/v1/ip-sets/' + rule['destinations'][0]['target_id'])
+                   # Convert JSON res
+                   # ponse ipset rule to python dict ipsetd
+                ipsetd = json.loads(ipset.text)
+                try:
+                 this['destination includes'] = [ipsetd][0]['ip_addresses']
+                except:
+                 this['destination includes'] = ' '
+         if rule.get('sources'):
+            if 'target_id' in rule['sources'][0]:
+                this['sources'] = rule['sources'][0]['target_display_name']
+                if 'IPSet' in rule['sources'][0]['target_type']:
+                    ipsets = nsx.get('https://' + nsx_manager + '/api/v1/ip-sets/' + rule['sources'][0]['target_id'])
+                        # Convert JSON response ipset rule to python dict ipsetd
+                    ipsetsd = json.loads(ipsets.text)
+                    try:
+                     this['sources includes'] = [ipsetsd][0]['ip_addresses']
+                    except:
+                     this['sources includes'] = ' '
+         csv_rule = ([this['id'], this['display_name'], this['services'], this['applied_tos'], this['action'], this['direction'],this['destination'], this['destination includes'], this['sources'], this['sources includes']])
+         csv_file.writerow(csv_rule)
+
+print("CSV: Closing file..")
+target.close()
+print("CSV: File closed.")
